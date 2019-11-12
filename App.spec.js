92c714bf73dfbe5d38bb33889eb3c3c1d2dbf67e
Helder Nascimento <helder@lifeaz.fr>
2019-11-12 17:12:04 +0100

    add View render test

---
+++ App.spec.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import App from './App';
+
+describe('App component', () => {
+  it('renders View', () => {
+    const AppComponent = renderer.create(<App />).toJSON();
+    expect(AppComponent).toMatchSnapshot();
+  });
+});
